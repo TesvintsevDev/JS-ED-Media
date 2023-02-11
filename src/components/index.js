5783e409e92c4b580978809acb2817c6be08d680
Eduard Tesvintsev <tesvintsev@gmail.com>
2023-02-11 19:57:56 +0300

    Navbar and Sidebar + API

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ export { default as Feed } from './Feed';
 export { default as Navbar } from './Navbar';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchFeed } from './SearchFeed';
-
+export { default as SearchBar } from './SearchBar';
+export { default as Videos } from './Videos';
+export { default as Sidebar } from './Sidebar';
